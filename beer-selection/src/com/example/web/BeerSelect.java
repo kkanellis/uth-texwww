55e63070f6bfcfe1f993b21e33f006574941aa37
Konstantinos Kanellis <kkanelli@hotmail.com>
2016-10-12 17:59:04 +0300

    Add servlet & correct BeerSelect.java

--- beer-selection/src/com/example/web/BeerSelect.java
+++ beer-selection/src/com/example/web/BeerSelect.java
@@ -5,20 +5,26 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.util.*;
 
+// Add the new model //
+import com.example.model.BeerExpert;
+
 public class BeerSelect extends HttpServlet {
 
- public void doPost(HttpServletRequest request,
-                    HttpServletResponse response)
-		    throws IOException, ServletException {
-       
-	response.setContentType("text/html");
-	PrintWriter out = response.getWriter();
-	out.println("Beer Selection Advice<br>");
-
+    public void doPost(HttpServletRequest request,
+                        HttpServletResponse response)
+			throws IOException, ServletException {
+        
         String c = request.getParameter("color");
-	out.println("<br>Got beer color " + c);
-
+        
+        BeerExpert be = new BeerExpert();
+        List result = be.getBrands(c);
+        
+        request.setAttribute("styles", result);
+        RequestDispatcher view =
+            request.getRequestDispatcher("result.jsp");
+        view.forward(request, response);
     }
+
 }
   
 
