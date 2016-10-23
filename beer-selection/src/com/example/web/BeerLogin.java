1fd811f7c22a8cb7db14ff7e2f75c3556959e767
Konstantinos Kanellis <kkanelli@hotmail.com>
2016-10-23 14:22:10 +0300

    Implement LDAP authentication

--- beer-selection/src/com/example/web/BeerLogin.java
+++ beer-selection/src/com/example/web/BeerLogin.java
@@ -5,10 +5,46 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.util.*;
 
+// Used for LDAP
+import javax.naming.*;
+import javax.naming.directory.*;
+
 // import listener
 import com.example.BeerSessionCounter;
 
 public class BeerLogin extends HttpServlet {
+
+	protected boolean doLogin(HttpServletRequest request,
+								HttpServletResponse response) 
+					throws ServletException, IOException {
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		Hashtable env = new Hashtable(11);
+		boolean success = false;
+
+		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+		env.put(Context.PROVIDER_URL, "ldap://ldap.uth.gr");
+		env.put(Context.SECURITY_AUTHENTICATION, "simple");
+		env.put(Context.SECURITY_PRINCIPAL, "uid=" + username + ",ou=people,dc=uth,dc=gr");
+		env.put(Context.SECURITY_CREDENTIALS, password);
+
+		try {
+			// Create initial context
+			DirContext ctx = new InitialDirContext(env);
+
+			// Close the context when we're done
+			success = true;
+			ctx.close();
+		}
+		catch (NamingException e) {
+			return false;
+		}
+	
+		return success;	
+	}
+
 
 	public void doGet(HttpServletRequest request,
 						HttpServletResponse response)
@@ -29,7 +65,7 @@ public class BeerLogin extends HttpServlet {
 			throws IOException, ServletException {
         
 		// Check if login is successfull
-		boolean success = true;
+		boolean success = doLogin(request, response); 
 		
 		if (success) {
 			// redirect to form.html
@@ -40,7 +76,8 @@ public class BeerLogin extends HttpServlet {
 			this.doGet(request, response);
 		}
     }
-
 }
   
+
+
 
